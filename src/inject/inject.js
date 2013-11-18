c59d1a625892d6b757b6011ee97da4659e514399
Matt Condon <MattCMultimedia@gmail.com>
2013-11-17 20:02:54 -0600

    templating is fun

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -19,11 +19,11 @@ chrome.extension.sendMessage({}, function(response) {
                     // '</div>'+
                     '<div class="feed-item-main">'+
                         '<div class="feed-item-header  vve-check">'+
-                             '<span class="feed-item-actions-line">Recent videos from <span class="feed-item-owner"><a href="/user/RoosterTeeth?feature=g-high-crv" class="g-hovercard yt-uix-sessionlink yt-user-name spf-nolink">Rooster Teeth</a> <span class="yt-user-name-icon-verified"><img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" class="yt-uix-tooltip yt-channel-title-icon-verified" alt="" /></span></span></span>'+
+                             '<span class="feed-item-actions-line">Recent videos from <span class="feed-item-owner"><a href="<%= feed.link[1].href %>" class="g-hovercard yt-uix-sessionlink yt-user-name spf-nolink"><%= feed.author[0].name %></a> <span class="yt-user-name-icon-verified"></span></span></span>'+
                         '</div>'+
                         '<div class="feed-item-main-content">'+
                             '<div class="shelf-wrapper clearfix">'+
-                                '<div class="compact-shelf shelf-item yt-uix-shelfslider yt-uix-shelfslider-at-head vve-check  feeds-mode yt-uix-tdl">'+
+                                '<div class="compact-shelf shelf-item yt-uix-shelfslider yt-uix-shelfslider-at-head vve-check feeds-mode yt-uix-tdl">'+
                                     '<div class="yt-uix-shelfslider-body context-data-container">'+
                                         '<ul id="othervids-list" class="yt-uix-shelfslider-list">'+
                                         '</ul>'+
@@ -46,29 +46,23 @@ chrome.extension.sendMessage({}, function(response) {
                             '<li class="channels-content-item yt-shelf-grid-item yt-uix-shelfslider-item ">'+
                                 '<div class="yt-lockup clearfix  yt-lockup-video yt-lockup-grid vve-check context-data-item">'+
                                     '<div class="yt-lockup-thumbnail">'+
-                                         '<a href="<%= link[0].href %>" class="ux-thumb-wrap yt-uix-sessionlink yt-uix-contextlink yt-fluid-thumb-link contains-addto ">'+
+                                         '<a href="<%= link[0].href %>" class="ux-thumb-wrap yt-uix-sessionlink yt-uix-contextlink yt-fluid-thumb-link contains-addto">'+
                                             '<span class="video-thumb  yt-thumb yt-thumb-175 yt-thumb-fluid"> <span class="yt-thumb-default">'+
                                             '<span class="yt-thumb-clip">'+
-                                            '<img src="//i1.ytimg.com/vi/rUSVNRE8MCo/mqdefault.jpg" alt="Thumbnail" width="175" />'+
+                                            '<img src="<%= _.findWhere(media$group.media$thumbnail, {width:120}).url %>" alt="Thumbnail" width="175" />'+
                                             '<span class="vertical-align"></span></span></span></span></a>'+
-                                        '<button class="addto-button video-actions spf-nolink hide-until-delayloaded addto-watch-later-button yt-uix-button yt-uix-button-default yt-uix-button-size-small yt-uix-tooltip" type="button">'+
-                                            '<span class="yt-uix-button-content"><img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="Watch Later" /></span>'+
-                                        '</button>'+
                                     '</div>'+
                                     '<div class="yt-lockup-content">'+
                                         '<h3 class="yt-lockup-title">'+
-                                             '<a class="yt-uix-sessionlink" title="Achievement Hunter Presents: PS4 Unboxing" href="/watch?v=rUSVNRE8MCo"><span class="yt-ui-ellipsis-wrapper">Achievement Hunter Presents: PS4 Unboxing</span></a>'+
+                                             '<a class="yt-uix-sessionlink" title="<%= title.t %>" href="/watch?v=rUSVNRE8MCo"><span class="yt-ui-ellipsis-wrapper"><%= title.t %></span></a>'+
                                         '</h3>'+
                                         '<div class="yt-lockup-meta">'+
                                             '<ul class="yt-lockup-meta-info">'+
                                                 '<li>'+
-                                                    'by <a href="/user/RoosterTeeth?feature=g-high-crv" class="g-hovercard yt-uix-sessionlink yt-user-name ">Rooster Teeth</a> <span class="yt-user-name-icon-verified"><img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" class="yt-uix-tooltip yt-channel-title-icon-verified" alt="" /></span>'+
+                                                    'by <a href="/user/RoosterTeeth?feature=g-high-crv" class="g-hovercard yt-uix-sessionlink yt-user-name "><%= author[0].name %></a> <span class="yt-user-name-icon-verified"></span>'+
                                                 '</li>'+
                                                 '<li>'+
-                                                    '460,239 views'+
-                                                '</li>'+
-                                                '<li class="yt-lockup-deemphasized-text">'+
-                                                    '2 days ago'+
+                                                    '<%= yt$statistics.viewCount %> views'+
                                                 '</li>'+
                                             '</ul>'+
                                         '</div>'+
@@ -104,10 +98,11 @@ chrome.extension.sendMessage({}, function(response) {
                 totalPages = Math.floor(vidCount/5);
                 console.log(data);
                 $(containerTemplate).insertBefore("#playlist");
-                $(".othervids-content").html(slider({}));
+                $(".othervids-content").html(slider(data));
                 vidCount = data.feed.entry.length;
 
                 _.each(data.feed.entry, function(e) {
+                    e.title.t = e.title.$t.slice(0, 60) + "...";
                     $("#othervids-list").append(itemTemplate(e));
                 });
 
